a664012ccb0fa6200fef7b69656d3de26fd9f56d
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-06-06 17:02:33 -0300

    criando tela de editar pet

--- src/pages/Pets/EditarPet/index.jsx
+++ src/pages/Pets/EditarPet/index.jsx
@@ -1,8 +1,16 @@
+import { Box, Center} from "@chakra-ui/react";
+import { FormPets } from "../../../components/FormPets";
 
 export default function Editar() {
     return (
-        <div>
-            <h2>Editar</h2>    
-        </div>
+        <Center>
+            <Box w="75%" h="100%">
+                <Box fontWeight="bold" fontSize="30px" mb={61} mt={10} pl={4}>Editar pet</Box>
+
+                <Box width={400} lineHeight={8}>
+                    <FormPets button="Salvar"/>
+                </Box>
+            </Box>
+        </Center>
     );
 }
