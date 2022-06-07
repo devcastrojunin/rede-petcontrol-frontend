1137f670ae597bfd556b5df899f10b0bf13ec86a
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-06-07 17:36:45 -0300

    criando botão de adicionar e adicionando rota

--- src/components/GlobalTable/index.jsx
+++ src/components/GlobalTable/index.jsx
@@ -1,27 +1,41 @@
-import { Table, TableCaption, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Button, Flex, Spacer, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { FaPlus } from "react-icons/fa";
 
-export default function GlobalTable({ tableHead }) {
+export default function GlobalTable({ tableHead, goToAdd }) {
     return (
-        <Table size='md' fontSize="14px">
-            <Thead>
-                <Tr>
-                    {tableHead.map((listaHead, index) => (
+        <>
+            <Flex>
+                <Spacer />
+                <Button rightIcon={<FaPlus />}
+                    colorScheme='teal'
+                    variant='outline'
+                    mb={8}
+                    fontWeight={700}
+                    onClick={goToAdd}>
+                    Adicionar Novo
+                </Button>
+            </Flex>
+            <Table size='md' fontSize="14px">
+                <Thead>
+                    <Tr>
+                        {tableHead.map((listaHead, index) => (
 
-                        <Th w="22%" key={index}>{listaHead}</Th>
+                            <Th w="22%" key={index}>{listaHead}</Th>
 
-                    ))}
-                </Tr>
-            </Thead>
+                        ))}
+                    </Tr>
+                </Thead>
 
-            <Tbody>
-                <Tr>
-                    <Td>Junior Castro</Td>
-                    <Td>teste@teste.com</Td>
-                    <Td>Rua teste, 1234, SP</Td>
-                    <Td>Meus Pets</Td>
-                </Tr>
-            </Tbody>
+                <Tbody>
+                    <Tr>
+                        <Td>Junior Castro</Td>
+                        <Td>teste@teste.com</Td>
+                        <Td>Rua teste, 1234, SP</Td>
+                        <Td>Meus Pets</Td>
+                    </Tr>
+                </Tbody>
 
-        </Table>
+            </Table>
+        </>
     )
 }
--- src/pages/Clientes/index.jsx
+++ src/pages/Clientes/index.jsx
@@ -1,21 +1,22 @@
-import { Box, Button, Center, Flex, Spacer} from "@chakra-ui/react";
+import { Box, Center} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import GlobalTable from "../../components/GlobalTable";
+
 
 export default function Clientes() {
 
+    const navigate = useNavigate();
     let tableHead = ["Nome", "Email", "Endereço", "Pets" ];
+
+    const goToAdd = () => navigate('/clientes/cadastrar')
     
     return (
         
         <Center>
             <Box w="75%" h="100%">
                 <Box fontWeight="bold" fontSize="30px" mb={61} mt={10} pl={4}>Todos os Clientes</Box>
-
-                <Flex>
-                    <Spacer/>
-                    <Button colorScheme='teal'>Sign Up </Button>
-                </Flex>
-                <GlobalTable tableHead={tableHead}/>
+                
+                <GlobalTable tableHead={tableHead} goToAdd={goToAdd}/>
             </Box>
         </Center>
         
