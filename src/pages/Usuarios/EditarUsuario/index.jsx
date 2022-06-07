51231bf93963b51b640318deef09b0e0508405bb
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-06-07 15:37:10 -0300

    criando tela de cadastro de usuario

--- src/pages/Usuarios/EditarUsuario/index.jsx
+++ src/pages/Usuarios/EditarUsuario/index.jsx
@@ -1,8 +1,16 @@
+import { Box, Center} from "@chakra-ui/react";
+import { FormUsuarios } from "../../../components/FormUsuarios";
 
-export default function Editar() {
+export default function Novo() {
     return (
-        <div>
-            <h2>Editar</h2> 
-        </div>
+        <Center>
+            <Box w="75%" h="100%">
+                <Box fontWeight="bold" fontSize="30px" mb={61} mt={10} pl={4}>Editar Usuário</Box>
+
+                <Box width={400} lineHeight={8}>
+                    <FormUsuarios button="Salvar"/>
+                </Box>
+            </Box>
+        </Center>
     );
 }
