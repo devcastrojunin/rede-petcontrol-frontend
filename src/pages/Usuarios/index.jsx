b7212e5ebf948ad399d94a86f8e6868181934cfb
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-06-07 17:39:54 -0300

    criando botão de adicionar e adicionando rota

--- src/pages/Usuarios/index.jsx
+++ src/pages/Usuarios/index.jsx
@@ -1,9 +1,14 @@
 import { Box, Center} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import GlobalTable from "../../components/GlobalTable";
 
 export default function Usuarios() {
 
+    const navigate = useNavigate();
+
     let tableHead = ["Nome", "Usuário", "Perfil", "Data de cadastro" ];
+
+    const goToAdd = () => navigate('/usuarios/cadastrar')
     
     return (
         
@@ -11,7 +16,7 @@ export default function Usuarios() {
             <Box w="75%" h="100%">
                 <Box fontWeight="bold" fontSize="30px" mb={61} mt={10} pl={4}>Todos os Usuários</Box>
 
-                <GlobalTable tableHead={tableHead}/>
+                <GlobalTable tableHead={tableHead} goToAdd={goToAdd}/>
             </Box>
         </Center>
         
